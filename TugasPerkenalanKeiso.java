e72055632ea530fe34e846834bde3edf6c368159
Keisokki <keisokki_wijaya_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:55:27 +0700

    Add files via upload

---
+++ TugasPerkenalanKeiso.java
@@ -0,0 +1,8 @@
+public class TugasPerkenalanKeiso{
+    public static void main(String[] args) {
+        System.out.println("Keisokki Al-Bara' Nandana Wijaya");
+        System.out.println("Sukoharjo kec.Kartasura Ngemplak Gebyok Garden Vista");
+        System.out.println("MTsN 2 Surakarta");
+        System.out.println("085877777245");
+    }
+}
